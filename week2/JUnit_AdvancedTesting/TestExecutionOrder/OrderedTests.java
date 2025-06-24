b902426589de1eb0c63980211a3a181ecbccdd60
Bharadwaj Kudaravalli <bharadwaja307@gmail.com>
2025-06-24 09:44:57 +0530

    implemented  TestExecutionOrder

---
+++ week2/JUnit_AdvancedTesting/TestExecutionOrder/OrderedTests.java
@@ -0,0 +1,25 @@
+package JUnit_AdvancedTesting.TestExecutionOrder;
+
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class OrderedTests {
+
+    @Test
+    @Order(2)
+    void testSecond() {
+        System.out.println("Second test executed");
+    }
+
+    @Test
+    @Order(1)
+    void testFirst() {
+        System.out.println("First test executed");
+    }
+
+    @Test
+    @Order(3)
+    void testThird() {
+        System.out.println("Third test executed");
+    }
+}
