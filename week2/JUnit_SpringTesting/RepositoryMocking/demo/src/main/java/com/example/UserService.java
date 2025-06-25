ea580892c6a1696d3908c927006eaeee454116ae
Bharadwaj Kudaravalli <bharadwaja307@gmail.com>
2025-06-25 21:53:21 +0530

    Implemented JUnit and Spring Testing with Repository Mocking using Maven

---
+++ week2/JUnit_SpringTesting/RepositoryMocking/demo/src/main/java/com/example/UserService.java
@@ -0,0 +1,17 @@
+package com.example;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
