3fc644e1d21cc303b51f979ba508eb00368aebc0
Bharadwaj Kudaravalli <bharadwaja307@gmail.com>
2025-07-24 20:24:47 +0530

    Created a function component, Applies style to components,Rendered a component

---
+++ week6/HOL-3/scorecalculatorapp/src/App.js
@@ -0,0 +1,14 @@
+import { CalculateScore } from "./Components/CalculateScore";
+
+function App(){
+  return(
+    <div>
+      <CalculateScore Name={"Bharadwaj"}
+      School={"Raghu Engg"}
+      total={284}
+      goal={3} />
+    </div>
+  );
+}
+
+export default App;
